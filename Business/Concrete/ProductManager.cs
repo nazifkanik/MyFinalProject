d8281f8dc48fde19979308a48fcd7926ada08aae
Nazif Kanık <nazifkanik@gmail.com>
2021-03-11 14:02:05 +0300

    11.03.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -19,6 +20,18 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //İş Kodları
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("Ürün İsmi en az 2 karakter olmalı");
+            }
+            _productDal.Add(product);
+            
+            return new SuccessResult("Ürün Eklendi");
+        }
+
         public List<Product> GetAll()
         {
             //İş Kodları
@@ -30,6 +43,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
