e448ae096c2e949a9a837c80c6c8190b0c412acc
Nazif Kanık <nazifkanik@gmail.com>
2021-03-11 12:15:34 +0300

    11.03.2021

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    //Generic Constraint
+    //class : referans tip olabilir
+    //Entity : IEntity veya IEntity implemente eden bir class olabilir
+    //new() : new ile oluşturulabilen bir nesne olabilir
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
