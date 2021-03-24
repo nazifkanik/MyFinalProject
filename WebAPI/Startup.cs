b7f016a5e7eb949d9d322221d6bba969eaba9b81
Nazif Kanık <nazifkanik@gmail.com>
2021-03-24 19:46:20 +0300

    24.03.2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -84,6 +84,8 @@ services.AddCors();
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
